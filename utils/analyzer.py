18ee0a2d75f9f7d217aca1a469093ebcebc28051
Yagami <steve_syk@hotmail.com>
2025-05-03 12:31:26 +0100

    03/05/2025 development, fix bugs

--- utils/analyzer.py
+++ utils/analyzer.py
@@ -46,7 +46,7 @@ def analyze_portfolio(target_date: object = None, data_source: object = 'SXAFI')
         from . import calculate_positions, calculate_market_values, calculate_realized_pnl
         from . import generate_report
         from . import DataLoader
-        data_loader = DataLoader(trade_history_paths)
+        data_loader = DataLoader(investment_dir)
         trades_df, enum_df = data_loader.load_trade_data(trade_history_paths, enum_path, target_date)
         # trades_df, enum_df = load_trade_data(trade_history_paths, enum_path, target_date)
         if trades_df is None or enum_df is None:
@@ -69,7 +69,7 @@ def analyze_portfolio(target_date: object = None, data_source: object = 'SXAFI')
         daily_pnl_result = generate_report(
             daily_pnl_data, total_market_value, total_pnl, total_cost, realized_pnl, latest_date)
         # 保存结果
-        data_loader.save_results(daily_pnl_result, investment_dir, trade_history_paths)
+        data_loader.save_results(daily_pnl_result, trade_history_paths)
 
     except Exception as e:
         print(f"分析过程中发生错误: {e}")
