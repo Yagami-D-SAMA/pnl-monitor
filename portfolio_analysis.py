9682c4deecf94af9a2dd0a535a1e01628a65d1cb
Yagami <steve_syk@hotmail.com>
2025-05-06 20:00:56 +0100

    06/05/2025 development

--- portfolio_analysis.py
+++ portfolio_analysis.py
@@ -13,18 +13,16 @@ from utils.analyzer import (
 if __name__ == "__main__":
     # 用法示例：
     # 1. 计算今日PnL
-    # analyze_portfolio(data_source='ALL')
+    analyze_portfolio(data_source='ALL', asset_type=True)
     # 2. 查看历史PnL
-    # load_historical_pnl('2025-04-02', data_source='ALL')
+    # load_historical_pnl('2025-05-02', data_source='ALL')
     # 3. 计算累计贡献度
-    calculate_cumulative_contribution('2025-01-01', '2025-04-19', data_source='ALL')
+    # calculate_cumulative_contribution('2023-01-01', '2025-05-02', data_source='ALL')
     # 4. 运行历史分析
-    # run_historical_analysis('2025-01-01', '2024-04-11')
+    # run_historical_analysis('2025-04-25')
 
 # TODO Compare against SP500 with chart, fix SP500 date issue
 # TODO Compare against SP500 in cumulative return series
 # TODO Calculate alpha over SP500 and major indices
-# TODO Calculate hit ratio and portfolio stats
-# TODO Display historical pnl
-# TODO Dividend Calculation
-# TODO Adding holding period
+# TODO Display historical pnl, and hist vol?
+# TODO add portfolio analysis, asset type contribution and country breakdown
