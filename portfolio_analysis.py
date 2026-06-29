5a8263a28ec2d73ceb3ac4e5f205a6d1e846f404
Yagami <steve_syk@hotmail.com>
2026-06-29 21:45:03 +0100

    29/06/2026 development

--- portfolio_analysis.py
+++ portfolio_analysis.py
@@ -32,15 +32,15 @@ if __name__ == "__main__":
     # todo display top 10 constituents perf of main stream indices every day
     # todo China US EU PMI/CPI/Macro data analysis
     # todo improve UI user to zoom in trade for detail
-    # todo '2026-06-26', need to re calculate price
-    analyze_portfolio('2026-06-26',data_source='ALL', asset_type=True)
+    # todo N/A, need to re calculate price
+    analyze_portfolio(data_source='ALL', asset_type=True)
     # stock_monitor(90)
     # run_portfolio_drawdown_monitor(running_date=None, lookback_period=90, data_source='ALL')
     # export_industry_price_returns()
     # analyze_portfolio_industry_percentiles()
     # stock_value_factor()
     # 3. 查看历史PnL
-    # load_historical_pnl('2026-06-22', data_source='ALL')
+    # load_historical_pnl('2026-06-29', data_source='ALL')
     # 4. 计算累计贡献度
     # calculate_cumulative_contribution('2025-12-31', '2026-06-26', data_source='ALL')
     # 5. 运行历史分析
